7c289ce640fd7ab98209548a49309b04fd78f6eb
Ivan Efremov <st8998@gmail.com>
2012-11-27 22:13:17 +0400

    Symbol#of realisation

--- lib/eel/core_ext/symbol.rb
+++ lib/eel/core_ext/symbol.rb
@@ -1,10 +1,31 @@
 class Symbol
 
-  delegate :gt, :desc, :asc, to: :attr
+  delegate :gt, :gteq, :lt, :lteq,
+           :eq, :in,
+           :desc, :asc,
+           to: :attr
 
   def attr
     attr = Arel::Attributes::Attribute.new
     attr.name = self
+    attr
+  end
+
+  def of val
+    relation = case val
+                 when Class
+                   val.arel_table
+                 when Symbol
+                   val.to_s.classify.constantize
+                 when String
+                   val.classify.constantize
+                 else
+                   raise "Can't use #{val.class} as a relation"
+               end
+
+    attr = Arel::Attributes::Attribute.new
+    attr.name = self
+    attr.relation = relation
     attr
   end
 
